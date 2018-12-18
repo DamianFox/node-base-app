f5e60eb975a48d4cfc894f0b6c420a68be4c8778
Damiano Fossa <d.fossa.91@gmail.com>
2018-12-18 23:11:20 +0100

    MongoDB configuration

---
+++ models/db.js
@@ -0,0 +1,50 @@
+var mongoose = require('mongoose');
+const db = "YouyDBName";
+const dburl = process.env.NODE_ENV === 'production' ? "/" : "mongodb://localhost:27017/" + db;
+var retry = null;
+mongoose.connect(dburl);
+
+
+// CONNECTION EVENTS
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose connected to ' + dburl);
+});
+mongoose.connection.on('error', (err) => {
+  console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected');
+});
+
+// CAPTURE APP TERMINATION / RESTART EVENTS
+// To be called when process is restarted or terminated
+function gracefulShutdown(msg, callback) {
+  mongoose.connection.close(() => {
+    console.log('Mongoose disconnected through ' + msg);
+    callback();
+  });
+}
+
+// For nodemon restarts
+process.once('SIGUSR2', () => {
+  gracefulShutdown('nodemon restart', function() {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+// For app termination
+process.on('SIGINT', () => {
+  gracefulShutdown('App termination (SIGINT)', function() {
+    process.exit(0);
+  });
+});
+// For Heroku app termination
+process.on('SIGTERM', () => {
+  gracefulShutdown('App termination (SIGTERM)', function() {
+    process.exit(0);
+  });
+});
+
+// SCHEMAS & MODELS
+// Add your model here
+// For example:
+// require('./user.model');
