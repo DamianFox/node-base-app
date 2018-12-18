af361f56cf34b61e0075bb3c52b3d1c9b47e40fa
Damiano Fossa <d.fossa.91@gmail.com>
2018-12-18 23:16:23 +0100

    Controllers example

---
+++ controllers/user.controller.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+// Example
+// var User = mongoose.model('User');
+
+
+// module.exports.usersGetAll = function(req, res) {
+
+//   console.log('GET the users');
+//   console.log(req.query);
+
+//   User
+//     .find()
+//     .exec(function(err, users) {
+//       console.log(err);
+//       if (err) {
+//         console.log("Error finding users");
+//         res
+//           .status(500)
+//           .json(err);
+//       } else {
+//         console.log("Found users", users.length);
+//         res
+//           .json(users);
+//       }
+//     });
+// };
